04cb8b52541054a2dfa5cf9ec5051d1fb6b5f397
Kyleds63 <kstoner63@gmail.com>
2017-04-04 13:46:56 -0500

    Updated Eventful.html to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,83 @@
-var Query;
+var query;
 var locations={
   title:[],
   latitude:[],
   longitude:[]
 };
+var splitStart;
+var splitEnd;
+var search;
 
 
+// need an ID in the index for query to grab from
 
-Query = "music";
+//when clicking on this prototype button, we can set the date.
+$('#submit').on("click", function(){
+
+  query = $("#queryUse :selected").attr("value");
+  search = $("#navSearchBox").val().trim();
+
+  //set dates from values in the calader input
+  var startDate = $('#startDate').val();
+  var endDate = $('#endDate').val();
+  //eventful processes dates as YYYYMMDD00. Adding 00 to the end of the numbers.
+  var finalStart = startDate + "00";
+  var finalEnd = endDate + "00";
+  //removing hyphens in the numbers generated from the calander so eventful can read them.
+  splitStart = finalStart.split('-').join('');
+  splitEnd = finalEnd.split('-').join('');
+  createEvents();
+  });
+
+//Here we are setting up our specs to create the search.
+
+
+//-----------------------------------------------------------------------------------//
+// To Do: 1. link query to HTML, default to music. 2.sort by as a variable, such as date?
+//
+
+
 function createEvents(){
 
   var Events = {
 
   app_key: "SGJgVSbsbgT3pbP3",
 
-  c: Query,
+  q: search,
+  
+  // Category
+  c: query,
+  
+  // Location
+  location: ("30.307182, -97.755996"),
+  within: 15,
+  
+  // Sort the events in order of popularity
+  sort_order: "popularity",
+  
+  // Start and End Date
+  "date": splitStart + "_" + splitEnd,
 
-  where: "Austin",
-
-  t: "This+Weekend"
-
-
+  // Number of Items to Pull Up
+  page_size: 50
   };
 
+  // This function does the searching
   EVDB.API.call("/events/search", Events, function(findEvents) {
 
-
-
-    for (i=0;i<findEvents.events.event.length;i++){
+    //Display the items on the page for testing purposes
+       console.log(findEvents.events.event.length);
+       for (i=0;i<findEvents.events.event.length;i++){
+       $("#placeholder").append("<br>");
+       //title of events
+       $("#placeholder").append(findEvents.events.event[i].title);
+       $("#placeholder").append("<br>");
+       //start time of events
+       $("#placeholder").append(findEvents.events.event[i].start_time);
+       $("#placeholder").append("<br>");
+       //location of events
+       $("#placeholder").append(findEvents.events.event[i].city_name);
+ 
       locations.title[i]     = findEvents.events.event[i].title;
       locations.latitude[i]  = findEvents.events.event[i].latitude;
       locations.longitude[i] = findEvents.events.event[i].longitude;
@@ -40,7 +90,7 @@ function createEvents(){
 
 }
 
-createEvents();
+
 
 
 var map;
